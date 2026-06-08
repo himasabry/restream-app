d16cca3983838f7d1ef35032f1ff54d1597219b6
himasabry <36902869+himasabry@users.noreply.github.com>
2026-06-09 01:26:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ let ffmpegProcesses = {};
 // 🎯 لوجو لكل قناة
 const logos = {
   ch1: "logo1.png",
-  ch2: "logo2.png",
-  ch3: "logo3.png",
-  ch4: "logo4.png",
-  ch5: "logo5.png",
+  ch2: "logo22.png",
+  ch3: "logo33.png",
+  ch4: "logo44.png",
+  ch5: "logo55.png",
 };
 
 function getLogo(id) {
