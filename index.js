aaa0e5d44ab8f626269fb819928dd12def39e8aa
himasabry <36902869+himasabry@users.noreply.github.com>
2026-06-12 22:01:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const channels = {
   },
 
   ch2: {
-    input: "http://stap2025.me:8080/live/nsbp46qijx/ukbyu5668f/146127.m3u8",
+    input: "https://streem.rodoye.com/live/rr/max2/index.m3u8",
     output: "rtmp://rtmp.livepeer.com/live/5716-lclm-8mhs-hd0n"
   },
 
