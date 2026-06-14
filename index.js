59570aff8d8ee0d4a998f84d83f2c63b92ea994f
himasabry <36902869+himasabry@users.noreply.github.com>
2026-06-14 23:13:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const channels = {
   },
 
   ch2: {
-    input: "https://streem.rodoye.com/live/rr/max2/index.m3u8",
+    input: "https://oottoo.online/max2g/video.m3u8",
     output: "rtmp://rtmp.livepeer.com/live/5716-lclm-8mhs-hd0n"
   },
 
