596c8ccb3a141beab0eb2cfacf10081a234cacb1
himasabry <36902869+himasabry@users.noreply.github.com>
2026-06-14 17:58:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.get("/start", (req, res) => {
 "-i", logo,
 
 "-filter_complex",
-"[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2[base];[1:v]scale=-1:3300[logo];[base][logo]overlay=main_w-overlay_w-5:5",
+"[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2[base];[1:v]scale=-1:3200[logo];[base][logo]overlay=main_w-overlay_w-5:5",
 
 "-c:v", "libx264",
 "-preset", "veryfast",
