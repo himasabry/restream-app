8b3f1be61eda9a18b57f0e90616ddf3a2fda0973
himasabry <36902869+himasabry@users.noreply.github.com>
2026-06-18 14:25:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function spawnStream(id) {
     "-i", getLogo(id),
 
     "-filter_complex",
-    "[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2[base];[1:v]scale=-1:3100[logo];[base][logo]overlay=W-w-2:2",
+    "[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2[base];[1:v]scale=-1:3000[logo];[base][logo]overlay=W-w-2:2",
 
     "-c:v", "libx264",
     "-preset", "veryfast",
